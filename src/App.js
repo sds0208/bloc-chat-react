7118e56ec304b8a6b442eb858203f24e6dd69696
sds0208 <sarahsantacruz86@gmail.com>
2018-01-21 12:28:45 -0600

    RoomList.js component added and rendered in App.js, firebase added, 3 fake rooms made in firebase database are now displayed on the RoomList

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as firebase from 'firebase';
+import RoomList from './components/RoomList';
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyDD885qsbGeezbWVCPwK2KpalprJHraVjU",
+  authDomain: "bloc-chat-react-69cac.firebaseapp.com",
+  databaseURL: "https://bloc-chat-react-69cac.firebaseio.com",
+  projectId: "bloc-chat-react-69cac",
+  storageBucket: "bloc-chat-react-69cac.appspot.com",
+  messagingSenderId: "575546249156"
+};
+firebase.initializeApp(config);
 
 class App extends Component {
   render() {
@@ -11,8 +24,8 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        < RoomList firebase={firebase} />
       </div>
     );
   }
