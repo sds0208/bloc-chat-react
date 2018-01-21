13bc56fc43f112ef6adb909aaf41236649f6475a
sds0208 <sarahsantacruz86@gmail.com>
2018-01-21 15:31:29 -0600

    can now create chat rooms that appear in real time in the browser and the firebase database

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -4,9 +4,12 @@ class RoomList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      rooms: []
+      rooms: [],
+      newRoomName: ''
     };
     this.roomsRef = this.props.firebase.database().ref('rooms');
+    this.handleChange = this.handleChange.bind(this);
+    this.createRoom = this.createRoom.bind(this);
   }
 
   componentDidMount() {
@@ -15,11 +18,28 @@ class RoomList extends Component {
       room.key = snapshot.key;
       this.setState({ rooms: this.state.rooms.concat( room ) });
     });
+
+  }
+
+  handleChange(event) {
+    event.preventDefault();
+    this.setState({newRoomName: event.target.value});
+  }
+
+  createRoom(event) {
+    event.preventDefault();
+    this.roomsRef.push({ name: this.state.newRoomName });
+    this.setState({ newRoomName: '' })
   }
 
   render() {
     return (
       <div className='roomlist'>
+        <form className="create-room" onSubmit={this.createRoom}>
+          <input type="text" value={this.state.newRoomName} onChange={this.handleChange}/>
+          <button type="submit">Create Room</button>
+        </form>
+
         {this.state.rooms.map(room =>
           <div className="room" key={room.key}>
             {room.name}
