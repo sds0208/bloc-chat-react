5c2b068fc82a9533bce2a73da73a0dc848b5b3cb
sds0208 <sarahsantacruz86@gmail.com>
2018-01-28 11:51:33 -0600

    user sign in through google/firebase added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import * as firebase from 'firebase';
 import RoomList from './components/RoomList';
 import MessageList from './components/MessageList';
+import User from './components/User';
 
 // Initialize Firebase
 var config = {
@@ -19,13 +20,18 @@ firebase.initializeApp(config);
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { activeRoom: '' };
+    this.state = { activeRoom: '', user: '' };
     this.activateRoom = this.activateRoom.bind(this);
   }
 
   activateRoom(room) {
     this.setState({ activeRoom: room });
     console.log(room);
+  }
+
+  setUser(user) {
+    this.setState({ user: user });
+    if (user !== null) {console.log(user.displayName)};
   }
 
   render() {
@@ -39,6 +45,7 @@ class App extends Component {
         </p>
         <main>
         </main>
+        < User firebase={firebase} setUser={this.setUser.bind(this)} user={this.state.user} />
         < RoomList firebase={firebase} activeRoom={this.state.activeRoom} activateRoom={this.activateRoom.bind(this)}/>
         < MessageList firebase={firebase} activeRoom={this.state.activeRoom} />
       </div>
---
+++ src/components/User.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+class User extends Component {
+
+  componentDidMount() {
+    this.props.firebase.auth().onAuthStateChanged( user => {
+      this.props.setUser(user);
+    });
+  }
+
+  signIn() {
+    const provider = new this.props.firebase.auth.GoogleAuthProvider();
+    this.props.firebase.auth().signInWithPopup(provider);
+  }
+
+  signOut() {
+    this.props.firebase.auth().signOut();
+  }
+
+  render() {
+
+    return (
+      <div className="user">
+        <h4 className="user-name">{ this.props.user ? 'Signed in as ' + this.props.user.displayName : 'Welcome, Guest!' }</h4>
+        <button className="sign-in-button" onClick={ this.props.user ?  this.signOut.bind(this) : this.signIn.bind(this)}>{ this.props.user? 'Sign Out' : 'Google Sign In'}</button>
+      </div>
+    );
+  }
+}
+
+export default User;
